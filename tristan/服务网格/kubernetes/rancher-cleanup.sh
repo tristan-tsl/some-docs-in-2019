176fe6d1a2e6c96dc564c2c35ec3b1133e4f5f68
tristan <3301015948@qq.com>
2019-09-18 15:24:45 +0800

    修复rancher cluster 节点清理问题

--- tristan/服务网格/kubernetes/rancher-cleanup.sh
+++ tristan/服务网格/kubernetes/rancher-cleanup.sh
@@ -5,6 +5,7 @@ mv /etc/kubernetes /etc/kubernetes-bak-$(date +"%Y%m%d%H%M")
 mv /var/lib/etcd /var/lib/etcd-bak-$(date +"%Y%m%d%H%M")
 mv /var/lib/rancher /var/lib/rancher-bak-$(date +"%Y%m%d%H%M")
 mv /opt/rke /opt/rke-bak-$(date +"%Y%m%d%H%M")
+for mount in $(mount  | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
 rm -rf /etc/ceph \
      /etc/cni \
      /opt/cni \
@@ -17,5 +18,3 @@ rm -rf /etc/ceph \
      /var/log/containers \
      /var/log/pods \
      /var/run/calico
-
-for mount in $(mount | grep tmpfs | grep '/var/lib/kubelet' | awk '{ print $3 }') /var/lib/kubelet /var/lib/rancher; do umount $mount; done
